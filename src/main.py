a724793c6aca1b362e3adf73bfa81f3c105afb43
Axel Montiel <axel.avi.fer@gmail.com>
2026-05-26 20:30:04 -0600

    feat: ejecutar fase 1 y 2

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import modules.phase1_2_universe as m1
+# import modulo3_preprocesamiento as m3  ← descomentar cuando esté listo
+
+RUTA_CSV      = './ENIGH_INEGI/viviendas.csv'
+CVE_ESTADO    = '15'
+NOMBRE_ESTADO = 'Estado de Mexico'
+ATRIBUTOS     = ['num_cuarto', 'tam_loc', 'calent_sol', 'p_fractura', 'regadera', 'tenencia']
+
+if __name__ == '__main__':
+
+    # Fase 1 y 2
+    df_estado = m1.ejecutar(RUTA_CSV, CVE_ESTADO, NOMBRE_ESTADO, ATRIBUTOS)
+
+    # Fase 3
+    # df_work = m3.ejecutar(df_estado, ATRIBUTOS)
